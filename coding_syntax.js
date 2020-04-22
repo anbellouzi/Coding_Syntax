dac0d5750b51de6326d6ecb5178481538414c962
Anas <anbellouzi@gmail.com>
2020-04-21 23:23:05 -0700

    coding syntax solved

---
+++ coding_syntax.js
@@ -0,0 +1,13 @@
+function FizzBuzz(targetnum) {
+    for(var i=1; i<targetnum; i++) {    
+        let result = "";
+        if (i%3 === 0) result += "Fizz";
+        if (i%5 === 0) result += "Buzz";
+        if (i%3 > 0 & i%5 > 0) result = i;
+        console.log(result += "\n");
+    }
+}
+
+
+
+FizzBuzz(50)
